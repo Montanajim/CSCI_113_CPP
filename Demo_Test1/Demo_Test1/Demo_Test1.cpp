10aab711a6696e0981b7bd0f245b40f3d610e4ab
Jim G. <jimgoudy@yahoo.com>
2015-01-19 15:48:10 -0700

    Test Folder
    Delete When Done

---
+++ Demo_Test1/Demo_Test1/Demo_Test1.cpp
@@ -0,0 +1,19 @@
+// Demo_Test1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char quit;
+
+	cout<<"Hello World";
+
+	cin>>quit;
+
+	return 0;
+}
+
